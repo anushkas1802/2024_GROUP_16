6c3a117c03b248e3bf7c71d625fc9c3c6b211b7c
IsaacDooley <isaacrd210205@gmail.com>
2025-05-05 18:35:21 +0100

    Added comments for optiondialog and model part

--- ModelPart.cpp
+++ ModelPart.cpp
@@ -1,59 +1,74 @@
+// Header file for this class
 #include "ModelPart.h"
+
+// Include VTK headers 
 #include <vtkSTLReader.h>
 #include <vtkPolyDataMapper.h>
 #include <vtkActor.h>
 #include <vtkProperty.h>
 #include <vtkPolyData.h>
 #include <vtkDataSetMapper.h>
+
+// Constructor: Initializes with data and an optional parent
 ModelPart::ModelPart(const QList<QVariant>& data, ModelPart* parent)
     : m_itemData(data), m_parentItem(parent) {
 }
 
+// Destructor: Deletes all child items to free memory
 ModelPart::~ModelPart() {
     qDeleteAll(m_childItems);
 }
 
+// Adds a child to this model part and sets its parent
 void ModelPart::appendChild(ModelPart* item) {
     item->m_parentItem = this;
     m_childItems.append(item);
 }
 
+// Returns a pointer to the child at the given row, or nullptr if out of range
 ModelPart* ModelPart::child(int row) {
     if (row < 0 || row >= m_childItems.size())
         return nullptr;
     return m_childItems.at(row);
 }
 
+// Returns the number of child items
 int ModelPart::childCount() const {
     return m_childItems.count();
 }
 
+// Returns the number of columns in the item data
 int ModelPart::columnCount() const {
     return m_itemData.count();
 }
 
+// Retrieves the data at the specified column
 QVariant ModelPart::data(int column) const {
     if (column < 0 || column >= m_itemData.size())
         return QVariant();
     return m_itemData.at(column);
 }
 
+// Sets the data at a specific column
 void ModelPart::setData(int column, const QVariant& value) {
     if (column < 0 || column >= m_itemData.size())
         return;
     m_itemData.replace(column, value);
 }
 
+// Returns the parent of this model part
 ModelPart* ModelPart::parentItem() {
     return m_parentItem;
 }
 
+// Returns the index of this item in the parent's child list
 int ModelPart::row() const {
     if (m_parentItem)
         return m_parentItem->m_childItems.indexOf(const_cast<ModelPart*>(this));
     return 0;
 }
 
+// Sets the RGB color of the part and updates the actor's visual appearance
 void ModelPart::setColour(const unsigned char R, const unsigned char G, const unsigned char B) {
     colourR = R;
     colourG = G;
@@ -64,10 +79,12 @@ void ModelPart::setColour(const unsigned char R, const unsigned char G, const un
     }
 }
 
+// Accessor methods for individual RGB color components
 unsigned char ModelPart::getColourR() const { return colourR; }
 unsigned char ModelPart::getColourG() const { return colourG; }
 unsigned char ModelPart::getColourB() const { return colourB; }
 
+// Sets visibility of the actor (part)
 void ModelPart::setVisible(bool visible) {
     isVisible = visible;
     if (stlActor) {
@@ -75,10 +92,12 @@ void ModelPart::setVisible(bool visible) {
     }
 }
 
+// Returns the visibility state of the actor
 bool ModelPart::visible() const {
     return isVisible;
 }
 
+// Loads an STL file and creates a corresponding VTK actor
 void ModelPart::loadSTL(QString fileName) {
     stlReader = vtkSmartPointer<vtkSTLReader>::New();
     stlReader->SetFileName(fileName.toStdString().c_str());
@@ -91,67 +110,43 @@ void ModelPart::loadSTL(QString fileName) {
     actor->SetMapper(stlMapper);
 
     this->stlActor = actor;
-
 }
 
+// Returns the existing VTK actor associated with this model part
 vtkSmartPointer<vtkActor> ModelPart::getActor() {
     return this->stlActor;
 }
 
+// Removes and deletes all children from this part
 void ModelPart::removeAllChildren() {
     qDeleteAll(m_childItems);
     m_childItems.clear();
 }
 
-// ? New methods
-
+// Returns the color of the part as a QColor object
 QColor ModelPart::getColor() const {
     return QColor(colourR, colourG, colourB);
 }
 
+// Sets the color of the part using a QColor object
 void ModelPart::setColor(const QColor& color) {
     setColour(color.red(), color.green(), color.blue());
 }
 
-//vtkActor* ModelPart::getNewActor() {
-    /* This is a placeholder function that you will need to modify if you want to use it
-     *
-     * The default mapper/actor combination can only be used to render the part in
-     * the GUI, it CANNOT also be used to render the part in VR. This means you need
-     * to create a second mapper/actor combination for use in VR - that is the role
-     * of this function. */
-
-
-     /* 1. Create new mapper */
-
-     /* 2. Create new actor and link to mapper */
-
-     /* 3. Link the vtkProperties of the original actor to the new actor. This means
-      *    if you change properties of the original part (colour, position, etc), the
-      *    changes will be reflected in the GUI AND VR rendering.
-      *
-      *    See the vtkActor documentation, particularly the GetProperty() and SetProperty()
-      *    functions.
-      */
-
-
-      /* The new vtkActor pointer must be returned here */
-  //    return nullptr;
-
-  //}
-
+// Creates and returns a new actor with the same geometry and property settings
 vtkActor* ModelPart::getNewActor() {
     if (!this->stlActor) {
         return nullptr;
     }
+
     newMapper = vtkSmartPointer<vtkDataSetMapper>::New();
     newMapper->SetInputConnection(stlReader->GetOutputPort());
 
     newActor = vtkSmartPointer<vtkActor>::New();
     newActor->SetMapper(newMapper);
 
+    // Copy visual properties from the original actor
     newActor->SetProperty(this->stlActor->GetProperty());
 
     return newActor;
 }
-
--- optiondialog.cpp
+++ optiondialog.cpp
@@ -1,23 +1,30 @@
+// Include the class header and associated UI form
 #include "optiondialog.h"
 #include "ui_optiondialog.h"
-#include<QDebug>
+
+// For debugging output
+#include <QDebug>
+
+// Constructor: sets up UI and initializes sliders, label, and connections
 OptionDialog::OptionDialog(QWidget* parent)
     : QDialog(parent)
     , ui(new Ui::OptionDialog)
 {
+    // Set up the UI generated from the .ui file
     ui->setupUi(this);
-    //ui->redSpinBox->setRange(0, 255);
-   // ui->greenSpinBox->setRange(0, 255);
-    //ui->blueSpinBox->setRange(0, 255);
 
+    // Fix the dialog size to 400x300 pixels
     this->setFixedSize(400, 300);
-    s_red = new QSlider(this);
-    s_red->setRange(0, 255);
-    s_red->setOrientation(Qt::Horizontal);
-    s_red->move(50,10);
-    s_red->setValue (10);
-    s_red->setFixedSize(300, 20);
 
+    // --- Create Red Slider ---
+    s_red = new QSlider(this);
+    s_red->setRange(0, 255);                   // Range for RGB values
+    s_red->setOrientation(Qt::Horizontal);     // Set horizontal layout
+    s_red->move(50, 10);                       // Position the slider
+    s_red->setValue(10);                       // Default value
+    s_red->setFixedSize(300, 20);              // Set slider size
+
+    // --- Create Green Slider ---
     s_green = new QSlider(this);
     s_green->setRange(0, 255);
     s_green->setOrientation(Qt::Horizontal);
@@ -25,82 +32,89 @@ OptionDialog::OptionDialog(QWidget* parent)
     s_green->setValue(10);
     s_green->setFixedSize(300, 20);
 
+    // --- Create Blue Slider ---
     s_blue = new QSlider(this);
-    s_blue ->setRange(0, 255);
+    s_blue->setRange(0, 255);
     s_blue->setOrientation(Qt::Horizontal);
     s_blue->move(50, 100);
     s_blue->setValue(10);
     s_blue->setFixedSize(300, 20);
 
+    // --- Create result color preview label ---
     res = new QLabel(this);
     res->setFixedSize(300, 30);
-        res->move(50, 150);
+    res->move(50, 150);
+    // Initial color: red with border
     res->setStyleSheet("QLabel{background-color:rgb(255,0,0);border:2px solid red;}");
 
+    // --- Connect sliders to their corresponding slots ---
     connect(s_red, SIGNAL(valueChanged(int)), this, SLOT(red_change()));
     connect(s_green, SIGNAL(valueChanged(int)), this, SLOT(green_change()));
     connect(s_blue, SIGNAL(valueChanged(int)), this, SLOT(blue_change()));
 
+    // Initialize string representations of RGB values
     r = QString::number(s_red->value());
     g = QString::number(s_green->value());
     b = QString::number(s_blue->value());
-
 }
 
+// Slot: Called when red slider value changes
 void OptionDialog::red_change()
 {
-    qDebug() << s_red->value();
-    r = QString::number(s_red->value());
-
+    qDebug() << s_red->value();                       // Print value to debug
+    r = QString::number(s_red->value());              // Update red value string
+    // Update label background color
     res->setStyleSheet("QLabel{background-color:rgb(" + r + ", " + g + ", " + b + "); }");
-
 }
 
+// Slot: Called when green slider value changes
 void OptionDialog::green_change()
 {
     qDebug() << s_green->value();
     g = QString::number(s_green->value());
-
     res->setStyleSheet("QLabel{background-color:rgb(" + r + ", " + g + ", " + b + "); }");
-
 }
 
+// Slot: Called when blue slider value changes
 void OptionDialog::blue_change()
 {
     qDebug() << s_blue->value();
     b = QString::number(s_blue->value());
-
     res->setStyleSheet("QLabel{background-color:rgb(" + r + ", " + g + ", " + b + "); }");
-
 }
+
+// Destructor: Deletes UI components to prevent memory leaks
 OptionDialog::~OptionDialog()
 {
     delete ui;
 }
 
+// Sets the initial values of the dialog's fields
 void OptionDialog::setValues(const QString& name, const QColor& color, bool visible)
 {
-    ui->nameLineEdit->setText(name);
-    ui->checkBox->setChecked(visible);
-    s_red->setValue(color.red());
-    s_green->setValue(color.green());
-    s_blue->setValue(color.blue());
+    ui->nameLineEdit->setText(name);                   // Set text in line edit
+    ui->checkBox->setChecked(visible);                 // Set visibility checkbox
+    s_red->setValue(color.red());                      // Set red slider
+    s_green->setValue(color.green());                  // Set green slider
+    s_blue->setValue(color.blue());                    // Set blue slider
 
-    res->setStyleSheet("QLabel{background-color:rgb(" + r + ", " + g + ", " + b + "); }"); 
+    // Update color preview label using existing RGB strings (already updated by sliders)
+    res->setStyleSheet("QLabel{background-color:rgb(" + r + ", " + g + ", " + b + "); }");
 }
 
-
+// Getter for the entered name
 QString OptionDialog::getName() const
 {
     return ui->nameLineEdit->text();
 }
 
+// Getter for selected color as QColor
 QColor OptionDialog::getColor() const
 {
     return QColor(s_red->value(), s_green->value(), s_blue->value());
-
 }
 
+// Getter for visibility checkbox state
 bool OptionDialog::isVisible() const
 {
     return ui->checkBox->isChecked();
